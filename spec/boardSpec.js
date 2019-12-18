30af2f4e0ced22874a1cd046065ce1c8b72a21d7
Amy Balmforth <amy.balmforth@gmail.com>
2019-12-18 14:42:20 +0000

    have a method for working out if game is a draw

--- spec/boardSpec.js
+++ spec/boardSpec.js
@@ -5,9 +5,13 @@ describe('Board', function() {
   });
 
   describe('get square function', function() {
-    it('returns the value of the square', function() {
+    it('returns the value of a square', function() {
       board.grid[0][0] = 'X'
       expect(board.getSquare(0, 0)).toEqual('X');
+    });
+    it('returns the value of a second square', function() {
+      board.grid[1][1] = 'O'
+      expect(board.getSquare(1, 1)).toEqual('O');
     });
   });
 
@@ -15,7 +19,27 @@ describe('Board', function() {
     it('sets the value of the square', function() {
       board.setSquare(0, 0, 'O');
       expect(board.getSquare(0, 0)).toEqual('O');
-      console.log(board.grid);
+    });
+    it('sets the value of a second square', function() {
+      board.setSquare(1, 0, 'X');
+      board.setSquare(2, 2, 'O');
+      expect(board.getSquare(1, 0)).toEqual('X');
+      expect(board.getSquare(2, 2)).toEqual('O');
+    });
+  });
+
+  describe('a draw', function() {
+    it('knows if the game is a draw', function() {
+      board.setSquare(0, 0, 'X');
+      board.setSquare(0, 1, 'O');
+      board.setSquare(0, 2, 'O');
+      board.setSquare(1, 0, 'O');
+      board.setSquare(1, 1, 'X');
+      board.setSquare(1, 2, 'X');
+      board.setSquare(2, 0, 'X');
+      board.setSquare(2, 1, 'X');
+      board.setSquare(2, 2, 'O');
+      expect(board.isDraw()).toEqual(true);
     });
   });
 
--- src/board.js
+++ src/board.js
@@ -8,6 +8,21 @@
   Board.prototype.setSquare = function(x, y, value) {
     this.grid[x][y] = value;
   };
+  Board.prototype.isDraw = function() {
+    // flatten to one array
+    var totalGrid = [].concat.apply([], this.grid);
+    var count = {};
+    totalGrid.forEach(x => count[x] = (count[x] || 0) +1);
+    // returns an object x:#
+    total = 0;
+    for (var property in count) {
+      total += count[property];
+    };
+    // total amount of x's and o's
+      if (total === 9) {
+        return true;
+      };
+  };
 
   exports.Board = Board;
 })(this);
